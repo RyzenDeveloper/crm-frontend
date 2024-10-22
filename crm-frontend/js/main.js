9f02072f4f5022b94eb0e7bea1a970b60565c874
TechnicalDeveloper <pavel@ntrmail.ru>
2024-10-22 13:14:52 +0300

    base url upd

--- crm-frontend/js/main.js
+++ crm-frontend/js/main.js
@@ -1,6 +1,6 @@
 const DELAY_TIME = 300;
 const VISIBLE_CSS = 'visible';
-const URI = 'http://crm-pro.cpl3.dev:3001/api/clients';
+const URI = 'https://crm-pro.cpl3.dev:3000/api/clients';
 
 const clientsState = {
   columnOfSort: 'id',
